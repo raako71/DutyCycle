484ac84d2867ea71c609d390afa64e47bec19244
raako71 <mail@raako.net>
2020-02-09 13:21:55 -0500

    Submariner CSS

--- index.php
+++ index.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>raako</title>
     <style>
-        p, .success2 {text-align: center;font-family: sans-serif;}
-        h1 {font-family: 'Indie Flower', cursive;text-align: center;}
-		#mainBox {max-width: 300px; margin: 30px auto; background-color: #dddddd;padding: 20px;}
-		.topBox { border-style: solid;border-width: 2px;}
-		.bottomBox {width: 80px; line-height: 30px; height: 30px; margin: auto}
+        a, p, h3, .success2 {text-align: center;font-family: monospace; color: #04ff00;}
+		a{color: black;}
+		input {background-color: black;color: #04ff00;border:1px solid #04ff00;text-align: center;}
+		#mainBox {max-width: 300px; margin: 30px auto;padding: 20px; border: solid #04ff00;}
+		.topBox { border-style: solid;border-width: 2px; border: solid #04ff00;}
+		.bottomBox {width: 80px; line-height: 30px; height: 30px; margin-top: 18px; margin-left: auto; margin-right: auto;}
 		input[type=text] { width: 50px;}
-		.success {background: #f3ff3c; padding: 2px;opacity:0; height: 50px;transition:height 3s, opacity 0.4s;}
-		.success2 {opacity:1; height: 34px; transition: opacity 1s, margin-block-start 1s, height 3s;, display: block; margin-block-start: 1em;}
+		.success2 {colour:red;background-color:white;}
 		#time {height:18px}
+		body { background: black;}
 		
     </style>
 	<?php
@@ -58,7 +59,6 @@
 			$result = hook($_POST["command"]);
 			$a = "Congratulations! You've fired the SwitchOn event";
 			$b = "Congratulations! You've fired the SwitchOff event";
-			//if($result == $a || $result == $b) echo "<br>Success";
 		}
 	function test_input($data) {
 		$data = trim($data);
@@ -85,25 +85,26 @@
 <body>
     <div id="mainBox">
 		<div class="topBox">
-        <p style="font-style: italic;">O3 Duty Cycle</p>
+        <h3 style="font-style: italic;">O3 Duty Cycle</h3>
 		<form method="post" class="redirect" action="<?php echo $_SERVER['PHP_SELF'];?>">
         <p>Run for <input type='text' value='<?php echo $array[runTime] ?>' name='runTime'>  minutes</p> 
 		<p>Sleep for <?php echo "<input type='text' value='$array[period]' name='period'/>"; ?> minutes</p> 
         <p>Active  <input type='checkbox' <?php if($array[active] == 1) echo "checked";?> value='true' name='active'/> </p>
-		<p><input type="submit" value="Submit"></p>
+		<p id='submit'>
+		<?php if ($change == 1) echo "UPDATED"; else echo '<input type="submit" value="Submit">';?>
+		</p>
 		<p id='time'> </p>
 		<!-- Next cycle in <?php echo $array[nextRun] - strtotime("now") ?> seconds -->
-		<div class="success"><p>Updated</p></div>
 		</div>
 		<div class="bottomBox" style="background-color: #0eff58">
-		<a href='javascript:void(0)' style="display: block" class='button' var='On'><p>run</p></a>
+		<a href='javascript:void(0)' style="display: block" class='button' var='On' id="RUN"><?php if($result == $a) echo '<div class="success2">Success</div>'; else echo 'RUN'; ?></a>
 		</div>
-		<?php if($result == $a) echo '<div class="success2">Success</div>'; ?>
+		
 		<div class="bottomBox" style="background-color: #ff2576">
-		<a href='javascript:void(0)' style="display: block" class='button' var='Off'><p>Stop</p></a>
-		<input type="hidden" class="post" name="command">
+		<a href='javascript:void(0)' style="display: block" class='button' var='Off' id="STOP"><?php if($result == $b) echo '<div class="success2">Success</div>'; else echo 'STOP'; ?></a>
 		</div>
-		<?php if($result == $b) echo '<div class="success2">Success</div>'; ?>
+		<input type="hidden" class="post" name="command">
+		
     </div>    
 
 <script type="text/javascript">
@@ -115,15 +116,13 @@ $(".button").click(function() {
 $(document).ready(show());
 
 function show(){
-<?php if ($change == 1) echo "$('.success').css('opacity', '1');"?>
-setTimeout(hide, 1000);
+
+setTimeout(hide, 4000);
 }
 function hide(){
-	$('.success').css('opacity', '0');
-	//$('.success').css('height', '0');
-	$('.success2').css('opacity', '0');
-	$('.success2').css('height', '0');
-	$('.success2').css('margin-block-start', '0');
+	document.getElementById('RUN').innerHTML = 'RUN';
+	document.getElementById('STOP').innerHTML = 'STOP';
+	document.getElementById('submit').innerHTML = '<input type="submit" value="Submit">';
 };
 
 // Update the count down every 1 second
@@ -145,4 +144,3 @@ function setTime(){
 </script>
 
 </body>
-
